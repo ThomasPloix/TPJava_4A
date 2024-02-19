694bf256c4d58ba5c945e08d80ed899072cce9b4
ThomasPloix <thomas.ploix@gmail.com>
2024-02-19 16:36:17 +0100

    Ajout CLI pas fini

---
+++ src/main/java/com/epf/rentmanager/ui/cli/Cli.java
@@ -0,0 +1,24 @@
+package com.epf.rentmanager.ui.cli;
+
+import com.epf.rentmanager.service.ClientService;
+import com.epf.rentmanager.service.VehicleService;
+
+import static com.epf.rentmanager.utils.IOUtils.print;
+
+public class Cli {
+    private ClientService clientService;
+    private VehicleService vehicleService;
+
+    public static void main(String[] args) {
+        print("Bonjour bienvenue sur le site de reservation ");
+        print("ALED");
+
+
+
+
+
+
+
+
+    }
+}
