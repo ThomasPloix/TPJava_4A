24c790aab61ec8c9ec02cb62ea0055dd72770848
ThomasPloix <thomas.ploix@gmail.com>
2024-03-04 16:33:57 +0100

    Ajout POM Junit création de test

---
+++ src/test/java/epf/rentmanager/service/TesteurVehiculeService.java
@@ -0,0 +1,7 @@
+package epf.rentmanager.service;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+@DisplayName("Tests associés au com.epf.rentmanager.service.VehiculeService")
+public class TesteurVehiculeService {
+
+}
